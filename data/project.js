97c4721af880e44a5d141e914f1e281c24f222c1
aqiuzzzz <2164468581@qq.com>
2016-09-27 15:31:53 +0800

    yk

---
+++ data/project.js
@@ -0,0 +1,34 @@
+
+/*		项目池
+	*   {
+	*       projectName: @value String            *         项目名称
+	*       projectWebsite: @value String         *         项目网址
+	*       startTime: @value String              *         项目开始时间  格式为：2016.10.09
+	*       endTime: @value String 			      *         项目结束时间  格式为：2016.10.09
+	*       projectExplain: @value String         *         项目说明
+	*       projectLabel: @value Array            *         项目技术标签 
+			projectThumbnail: @value String       *         缩略图地址
+	*       
+	*   }
+*/
+
+var projects = [
+		{
+			projectName:"整屏切换+动画制作“TweenMax库”",
+			projectWebsite:"http://aqiuzzzz.github.io/github1/works index",
+		    startTime:"2016.01.01",
+			endTime:"至今",
+			projectExplain:"JS整站开发之：整屏切换+动画制作神器“TweenMax库”  ,滚轮事件",
+			projectLabel:["html","css3","css","js"],
+			projectThumbnail:"images/icon/prject-5.png"
+		},
+		{
+			projectName:"2016.9.20",
+			projectWebsite:"http://aqiuzzzz.github.io/github2/index",
+		    startTime:"2016.01.01",
+			endTime:"至今",
+			projectExplain:"监控滚轮位置，随滚轮定位的图片，空间感十足。",
+			projectLabel:["html","css3","css","js"],
+			projectThumbnail:"images/icon/project-6.png"
+		}
+]
---
+++ data/userInfo.js
@@ -0,0 +1,36 @@
+
+/*个人信息说明，数据格式为对象
+	*   {
+	*       userName: @value String               *         姓名
+			userPortrait: @value String           *         头像
+	*       jobWant: @value String                *         求职意向
+	*       userQQ: @value String|Number          *         QQ号码
+	*       userEmail: @value String              *         邮箱地址
+	*       motto: @value String                  []         激励格言
+	*       userPhone: @value String|Number       *          手机号码
+	*       userOrignPlace: @value String         *         籍贯
+	*       userSeatPlace: @value String          *         所在地
+	*       userWeibo：@value String              []         微博地址
+	*       userAssessment: @value String         []        自我评价
+	*       userHobby: @value Array              []          爱好
+	*       userAward:@value Array               []          奖项
+	*
+	*
+	*   }
+*/
+
+var userInfo = {
+    userName: "杨凯秋" , 
+    userPortrait:"images/icon/intro_03.png",
+    jobWant:  "WEB前端开发工程师",
+    userQQ:  "985842393",
+    userEmail:   "985842393@qq.com",
+    motto:  "置气和悲伤的源头都在于自己，你不听不闻不看，选择权在你置气和悲伤的源头都在于自己，你不听不闻不看，选择权在你置气和悲伤的源头都在于自己，你不听不闻不看，选择权在你置气和悲伤的源头都在于自己，你不听不闻不看，选择权在你", 
+    userPhone: 15243724727,
+    userOrignPlace:  "湖南省益阳沅江市" ,
+    userSeatPlace:  "北京市",
+    userAssessment: "丰富界面及效果实现，精通各种JS技巧与简化，为人和善。",   
+    userHobby: ["运动","游戏","电影"],
+}
+
+68
